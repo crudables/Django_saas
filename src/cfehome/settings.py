e9fff44c1f20fc89d88d83965ad6dd246e16e158
Saheed Awolaju <crudables@yahoo.com>
2025-02-09 03:20:50 +0000

    adding sauce

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=-0@dad(06e$kv0p1*w!2yjh_n@-9q+*2yq#r4s9xw902it3jn'
+SECRET_KEY = config("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+# DEBUG = str(os.environ.get("DJANGO_DEBUG")).lower() =="true"
+DEBUG = config("DJANGO_DEBUG", cast=bool)
+print("DEBUG ", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [
     ".railway.app" #https://saas.prod.railway.app
